2e0b5ef137c61497db93f1b6a09ca1365a7121e6
anup kumar reddy <kumarreddy002@gmail.com>
2018-05-19 17:19:48 +0530

    Change model to allow null values in table (Fundamentals) Added unit test to perform basic model access test

--- thanos/tests.py
+++ thanos/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from .models import Fundamentals, Symbols
+import logging
+from datetime import datetime
 
-# Create your tests here.
+
+class FundmentalsUnitTests (TestCase):
+    """Add simple access test for model (Fundamnetals) in thanos app"""
+
+    def setUp(self):
+        logging.basicConfig(level=logging.INFO)
+
+    def test_add_entry(self):
+        symbol = Symbols()
+        symbol.symbol_name = "NOCIL"
+        logging.info("Symbol is ( %s )", symbol.symbol_name)
+        symbol.market_name = Symbols.MARKETS[0][0]
+        logging.info("Symbol Belongs to ( %s )", symbol.market_name)
+        symbol.save()
+        fundamentals = Fundamentals()
+        fundamentals.symbol = symbol
+        fundamentals.year = datetime.strptime("2017 01 01", "%Y %m %d")
+        logging.info("The year field is set to ( %s )", fundamentals.year)
+        fundamentals.save()
+        self.assertEqual(1, Fundamentals.objects.all().count())
+
