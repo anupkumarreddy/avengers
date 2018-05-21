fb3bd851539061800d494cb9d10553c2ef25df99
anup kumar reddy <kumarreddy002@gmail.com>
2018-05-21 18:52:57 +0530

    Add cash flow statement extraction

--- ebony_maw/tests.py
+++ ebony_maw/tests.py
@@ -45,6 +45,15 @@ class MawViewTests(TestCase):
         logging.info("Profit and Loss Statement numbers extracted are ...")
         funda.extract_profit_and_loss_statement()
         self.assertEqual(751.33, float(funda.profit_loss_statement['Total Revenue']['2017']),
-                         "Balance Sheet mismatch, Expected : ( {} ), Actual : ( {} )".format(751.33,
+                         "Profit and Loss  mismatch, Expected : ( {} ), Actual : ( {} )".format(751.33,
                           funda.profit_loss_statement['Total Revenue']['2017']))
+
+    def test_extract_cash_flow_statement(self):
+        funda = FundamentalsExtractor(silent=False)
+        funda.url = "file:///home/anup/Downloads/cash_flow.html"
+        logging.info("Cash Flow Statement numbers extracted are ...")
+        funda.extract_cash_flow_statement()
+        self.assertEqual(140.64, float(funda.cash_flow_statement['Net CashFlow From Operating Activities']['2017']),
+                         "Cash Flow statement mismatch, Expected : ( {} ), Actual : ( {} )".format(140.64,
+                          funda.cash_flow_statement['Net CashFlow From Operating Activities']['2017']))
 
