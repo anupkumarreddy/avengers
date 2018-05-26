940406533b6ed3399885b03469279f597411f5f4
anup kumar reddy <kumarreddy002@gmail.com>
2018-05-26 09:56:12 +0530

    Remove redundant comments

--- ebony_maw/urls.py
+++ ebony_maw/urls.py
@@ -2,7 +2,5 @@ from django.conf.urls import url
 from .views import MawMainPage
 
 urlpatterns = [
-    # url('all_symbols/', MawMainPage.as_view(template_name='maw/all_symbol.html'), name='all_symbols'),
-    # url('symbol/', MawMainPage.as_view(template_name='maw/symbol.html'), name='symbol'),
     url('', MawMainPage.as_view(template_name='maw/dashboard.html'), name='dashboard'),
 ]
